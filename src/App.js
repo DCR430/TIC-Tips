e546639c702002a67ce872e450199a08c8dc2f5c
DCR430 <daniel.c.reyes30@gmail.com>
2021-10-13 16:53:45 -0400

    added_myths

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Facts from './Components/facts';
 import Header from './Components/header'
 import "./Styling/header.css"
 import "./Styling/facts.css"
+import "./Styling/myths.css"
+import Myths from './Components/myths';
 
 function App() {
   return (
    <div>
     <Header></Header>
     <Facts></Facts>
+    <Myths></Myths>
    </div>
     
   );
---
+++ src/Components/myths.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function Myths() {
+    return (
+        <div className="myths-heading">
+            <h3>Common Myths</h3>
+            <div>
+            <h4>Myth</h4>
+
+            <p>People who stutter are stupid, and lack intelligence</p>
+            </div>
+            <div>
+            <h4>Truth</h4>
+            <p>I am capable and will get a job in tech, no matter what it takes.</p>
+            </div>
+            
+        </div>
+    )
+}
