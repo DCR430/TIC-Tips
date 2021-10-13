5b1aaea30bc170e767eaa2f11cda92fcf36fa039
DCR430 <daniel.c.reyes30@gmail.com>
2021-10-13 16:57:30 -0400

    added_myth_css

--- src/Components/myths.js
+++ src/Components/myths.js
@@ -4,12 +4,12 @@ export default function Myths() {
     return (
         <div className="myths-heading">
             <h3>Common Myths</h3>
-            <div>
+            <div className="myth">
             <h4>Myth</h4>
 
             <p>People who stutter are stupid, and lack intelligence</p>
             </div>
-            <div>
+            <div className="fact">
             <h4>Truth</h4>
             <p>I am capable and will get a job in tech, no matter what it takes.</p>
             </div>
